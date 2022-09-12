31435ddbda80ddd5916f3327096c09bd5937d026
SHREYASH MANDAVKAR <72700552+shrey-0407@users.noreply.github.com>
2022-09-12 11:46:04 +0530

    Time: 4 ms (79.96%), Space: 9.3 MB (55.11%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& s) {
+        string ans;
+        sort(s.begin(),s.end());
+        string a=s[0];
+        string b=s[s.size()-1];
+        
+        for(int i=0;i<a.size();i++){
+            if(a[i]==b[i]){
+                ans+=a[i];
+            }else break;
+        }
+        return ans;
+    }
+};
