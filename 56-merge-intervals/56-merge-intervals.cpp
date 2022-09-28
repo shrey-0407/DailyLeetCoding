6968adc0efbc05170b28dc372b8ed20ff7b8baed
SHREYASH MANDAVKAR <72700552+shrey-0407@users.noreply.github.com>
2022-09-28 09:43:48 +0530

    Time: 98 ms (14.27%), Space: 19.1 MB (69.19%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& t) {
-       
         sort(begin(t),end(t));
-         int s=t[0][0];
+        int s=t[0][0];
         int e=t[0][1];
         vector<vector<int>>v;
         for(int i=1;i<t.size();i++){
@@ -14,10 +13,11 @@ public:
                 v.push_back({s,e});
                 s=t[i][0];
                 e=t[i][1];
+                
             }
-        
+           
         }
-        v.push_back({s,e});
+         v.push_back({s,e});
         return v;
     }
 };
