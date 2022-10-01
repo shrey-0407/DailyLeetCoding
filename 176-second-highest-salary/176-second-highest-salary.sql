53a6a9d9e77a324e1611333b38eb2a23e5054cd8
SHREYASH MANDAVKAR <72700552+shrey-0407@users.noreply.github.com>
2022-10-02 00:21:42 +0530

    Time: 421 ms (18.02%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,3 +1,8 @@
-select distinct max(salary)
-as SecondHighestSalary from employee as a
-where salary <(select max(salary) from employee as b where a.salary<b.salary);
+# select distinct max(salary)
+# as SecondHighestSalary from employee as a
+# where salary <(select max(salary) from employee as b where a.salary<b.salary);
+
+
+select max(e1.salary) as SecondHighestSalary
+from employee e1 join employee e2
+on e1.salary<e2.salary;
